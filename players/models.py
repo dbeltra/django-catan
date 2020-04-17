a9adb2bdf18fac5cc8b758a681b32d7b6f038f7b
David Beltra <dbe@pep.security>
2020-04-17 14:38:31 +0200

    add Player model

--- players/models.py
+++ players/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
 
 # Create your models here.
+
+class Player(models.Model):
+    created     = models.DateTimeField(editable=False)
+    modified    = models.DateTimeField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.email
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Player, self).save(*args, **kwargs)
+
+@receiver(post_delete, sender=Player)
+def _player_post_delete(sender, instance, **kwargs):
+    instance.user.delete()
